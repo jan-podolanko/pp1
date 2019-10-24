3dd7581c4c0d16e61b30c5e241d8df49e4f2020e
jan-podolanko <56258603+jan-podolanko@users.noreply.github.com>
2019-10-24 17:04:39 +0200

    Rename nr rach.py to zadanie 25.py

---
+++ 01-TypesAndVariables/zadanie 25.py
@@ -0,0 +1,6 @@
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
+nr_rach = input('Podaj numer rachunku: ')
+
+print(nr_rach[0:2] + ' ' + nr_rach[2:6] + ' ' + nr_rach[6:10] + ' ' + nr_rach[10:14] + ' ' + nr_rach[14:18] + ' ' + nr_rach[18:22] + ' ' + nr_rach[22:26])
